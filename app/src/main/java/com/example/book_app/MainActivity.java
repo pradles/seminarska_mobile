74db69d8dfeaf37cb7d39c3ac35e47650588a8e6
pradles <jernej.ivancic2002@gmail.com>
2023-01-02 17:31:19 +0100

    first commit

---
+++ app/src/main/java/com/example/book_app/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.book_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private TextView osebe;
+    private String url = "https://knjiznicanejc.azurewebsites.net/api/Avtor";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        osebe = (TextView) findViewById(R.id.osebe);
+    }
+
+    public static final String EXTRA_MESSAGE = "com.example.book_app.MESSAGE";
+
+    public void addKnjiga (View view) {
+        Intent intent = new Intent(this,AddKnjiga.class);
+        String message = "Dodaj knjigo v seznam.";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void prikaziOsebe(View view){
+        if (view != null){
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener)
+            {
+                @Override
+                public Map<String, String> getHeaders () throws AuthFailureError
+                {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("ApiKey", "SecretKey");
+                    return params;
+                }
+            };
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response){
+            ArrayList<String> data = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++){
+                try {
+                    JSONObject object =response.getJSONObject(i);
+                    String name = object.getString("authorName");
+                    String genre = object.getString("authorLastName");
+                    String date = object.getString("age");
+
+                    data.add(name + " " + genre + " " + date);
+
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    return;
+
+                }
+            }
+
+            osebe.setText("");
+
+
+            for (String row: data){
+                String currentText = osebe.getText().toString();
+                osebe.setText(currentText + "\n\n" + row);
+            }
+
+        }
+
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
+
+
+
+}
